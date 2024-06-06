389d84a0e5cfdef7d30aeaf2ca54f2e93a980239
OmarInCS <omarincs@gmail.com>
2024-06-06 09:41:44 +0300

    Day 08, Session 1

--- HrApiDay08/src/main/java/org/example/MyResource.java
+++ HrApiDay08/src/main/java/org/example/MyResource.java
@@ -1,31 +1,36 @@
 package org.example;
 
+import jakarta.inject.Singleton;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.UriInfo;
 
+import java.time.LocalDate;
+
 /**
  * Root resource (exposed at "myresource" path)
  */
+
+@Singleton
 @Path("myresource")
 public class MyResource {
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
+    int count = 0;
+//    @HeaderParam("apiKey") String apiKey; // Error
+//    @CookieParam("username") String username; // Error
+    @Context HttpHeaders headers;
+    @Context UriInfo uriInfo;
+
+
     @GET
-    @Path("{myVar}")
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt(
             @HeaderParam("apiKey") String apiKey,
-            @CookieParam("username") String username,
-            @Context HttpHeaders headers,
-            @Context UriInfo uriInfo
+            @CookieParam("username") String username
+//            , @Context HttpHeaders headers,
+//            @Context UriInfo uriInfo
             ) {
 
         System.out.println(headers.getDate());
@@ -38,9 +43,18 @@ public class MyResource {
         System.out.println(uriInfo.getQueryParameters());
         System.out.println(uriInfo.getQueryParameters().get("locId"));
 
-        return "Got it! name: " + username + " , apiKey: " + apiKey;
+        count += 1;
+
+        return "Got it! " + count + " name: " + username + " , apiKey: " + apiKey;
     }
 
+
+    @GET
+    @Path("{day}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getDate(@PathParam("day") LocalDate day) {
+        return "The date is: " + day;
+    }
 
 
 }
